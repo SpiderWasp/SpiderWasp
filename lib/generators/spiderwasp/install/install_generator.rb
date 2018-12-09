464f5dfd5c1375ec99e138d6862f363d430ffdfc
Aleksey Ivanov <mov.stox@gmail.com>
2018-12-09 17:24:47 +0200

    Minor updates to generator

--- lib/generators/spiderwasp/install/install_generator.rb
+++ lib/generators/spiderwasp/install/install_generator.rb
@@ -6,8 +6,8 @@ class Spiderwasp::InstallGenerator < Rails::Generators::Base
     initializer "spiderwasp.rb" do
       %q(require 'redis'
 
-uri = URI.parse(ENV["SPIDERWASP_REDIS_URL"] || "redis://localhost:6379/" )
-$redis_conn = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+uri = URI.parse(ENV['SPIDERWASP_REDIS_URL'] || 'redis://localhost:6379/' )
+$redis_conn = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 
 Rack::Attack.blocklisted_response = lambda do |env|
   # Using 503 because it may make attacker think that they have successfully
@@ -16,7 +16,7 @@ Rack::Attack.blocklisted_response = lambda do |env|
 end
 
 Rack::Attack.blocklist('block misbehaving clients via Spiderwasp') do |req|
-  is_in_blacklist = ($redis_conn.get("spiderwasp:drop:#{req.ip}") == "true")
+  is_in_blacklist = ($redis_conn.get("spiderwasp:drop:#{req.ip}") == 'true')
   Rails.logger.info { "[spiderwasp] #{req.ip} - #{req.path} - #{(is_in_blacklist ? 'deny' : 'allow')} - #{req.user_agent}"}
   is_in_blacklist
 end
